5affb31e8fb337864cb1ebba2a83250502d936f8
geet-we45 <geet.hirawat@we45.com>
2025-07-24 17:30:42 +0530

    vars

--- hosting/variables.tf
+++ hosting/variables.tf
@@ -1,14 +1,17 @@
 variable "environment" {
   description = "Environment name (e.g., dev, staging, prod)"
   type        = string
+  default     = "dev"
 }
 
 variable "custom_ami_name" {
-  description = "Name pattern of AMI to use for the instance"
+  description = "Name for the custom AMI"
   type        = string
+  default     = "amzn2-ami-hvm-*-x86_64-gp2"
 }
 
 variable "custom_ami_owner" {
-  description = "Owner ID of the AMI (e.g., 'amazon' for Amazon Linux or AWS account ID for custom AMI)"
+  description = "Owner of the custom AMI"
   type        = string
+  default     = "137112412989"
 }
